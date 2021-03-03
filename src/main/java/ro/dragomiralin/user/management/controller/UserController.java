b68d5e3763f273e747ce73286fcd41a41ba54d1d
Dragomir Alin <dragomirdanielalin@gmail.com>
2021-03-03 21:15:51 +0200

    fix

--- src/main/java/ro/dragomiralin/user/management/controller/UserController.java
+++ src/main/java/ro/dragomiralin/user/management/controller/UserController.java
@@ -2,6 +2,7 @@ package ro.dragomiralin.user.management.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ro.dragomiralin.user.management.repository.persistence.User;
@@ -19,5 +20,10 @@ public class UserController {
                 .email("smartfactory@dragomiralin.ro")
                 .username("smartfactory")
                 .build();
+    }
+
+    @PostMapping
+    public User create(){
+        return User.builder().build();
     }
 }
